d61f0d44f14eb23cab04f0f9c66ee6cf06765b4a
ReneB85 <grafik12@grafik12s-iMac.local>
2017-09-12 12:19:15 +0200

    Loginform with clientside validation

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,12 +1,21 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import LoginForm from "../forms/LoginForm";
 
-const LoginPage = () => 
-(
-    <div>
-        <h1>Login Page</h1>
-        <Link to="/" >Back</Link>
-    </div>
-);
-
+class LoginPage extends React.Component{
+    
+    submit = (data) => {
+        console.log(data);
+    };
+    
+    render(){
+        return(
+            <div>
+                <h1>Login Page</h1>
+                <LoginForm submit={this.submit} />
+            </div>
+        );
+        
+    }
+}
+console.log('jdsfis');
 export default LoginPage;
