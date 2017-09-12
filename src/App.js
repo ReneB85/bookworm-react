28ad0168280fe39fc696702001ccc08856016abb
ReneB85 <grafik12@grafik12s-iMac.local>
2017-09-12 10:54:20 +0200

    Semantic UI setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
 
-
-
 const App = () => ( 
     <BrowserRouter>
-        <Switch>
+        <div className="ui container">
             <Route path="/" exact component={HomePage} />
             <Route path="/login" exact component={LoginPage} />
-        </Switch>
+        </div>
     </BrowserRouter>
 );
 
