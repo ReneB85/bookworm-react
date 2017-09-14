a61545675b5dc536ff07b192d5a1d2ac0de4c98a
ReneB85 <grafik12@grafik12s-iMac.local>
2017-09-14 12:04:17 +0200

    noch ein git test

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -4,8 +4,7 @@ import {Link} from "react-router-dom";
 
 const HomePage = () => (
     <div>
-        <h1>Home Page
-        </h1>
+        <h1>Home Page</h1>
         <Link to="/login">Login</Link>
     </div>
 );
