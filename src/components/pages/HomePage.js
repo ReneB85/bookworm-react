e179f4b8dbd045fcfcd4e8ab75bd19939a4dc270
ReneB85 <grafik12@grafik12s-iMac.local>
2017-09-14 12:02:58 +0200

    Remote Repository test

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 
 const HomePage = () => (
     <div>
-        <h1>Home Page</h1>
+        <h1>Home Page GitTest</h1>
         <Link to="/login">Login</Link>
     </div>
 );
